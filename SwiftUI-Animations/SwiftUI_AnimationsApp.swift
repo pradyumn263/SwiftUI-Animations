1cc1d06139d527971e32831293e23183732387ec
Pradyumn Shukla <ps811876@student.nitw.ac.in>
2021-01-06 13:38:38 +0530

    Initial Commit

---
+++ SwiftUI-Animations/SwiftUI_AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AnimationsApp.swift
+//  SwiftUI-Animations
+//
+//  Created by Pradyumn Shukla on 06/01/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
