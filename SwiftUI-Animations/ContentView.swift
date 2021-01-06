f3e8d5ffb960bd7f37b132c4a837bf40e9f6016a
Pradyumn Shukla <ps811876@student.nitw.ac.in>
2021-01-06 15:42:20 +0530

    Heart Animation (Explicit and Implicit)

--- SwiftUI-Animations/ContentView.swift
+++ SwiftUI-Animations/ContentView.swift
@@ -8,9 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var circleColorChanged = false
+    @State private var heartColorChanged = false
+    @State private var heartSizeChanged = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Circle()
+            .frame(width: 200, height: 200)
+            .foregroundColor(circleColorChanged ? Color(.systemGray5) : .red)
+            .overlay(
+                Image(systemName: "heart.fill")
+                    .font(.system(size: 100))
+                    .foregroundColor(heartColorChanged ? .red : .white)
+                    .scaleEffect(heartSizeChanged ? 1 : 0.6)
+            )
+            .onTapGesture {
+                withAnimation(.spring(response: 0.3, dampingFraction: 0.3, blendDuration: 0.3)) {
+                    heartSizeChanged.toggle()
+
+                }
+                circleColorChanged.toggle()
+                heartColorChanged.toggle()
+
+            }
+            
     }
 }
 
